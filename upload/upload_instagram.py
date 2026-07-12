49698f35aa705fe303019ca12400c9c936b13b2b
gal-gadot-goddess <gal_gadot.goddess@proton.me>
2026-07-12 08:06:28 +0530

    Fix GitHub URL for account with hyphens

--- upload/upload_instagram.py
+++ upload/upload_instagram.py
@@ -144,7 +144,7 @@ def upload_to_instagram(video_path, caption, is_story=False):
             if _ret == 0:
                 break
             time.sleep(5)
-        video_url = "https://raw.githubusercontent.com/" + gal-gadot-goddess + "/" + day3-on-map-algo + "/main/" + _vid_name
+        video_url = "https://raw.githubusercontent.com/gal-gadot-goddess/day3-on-map-algo/main/" + _vid_name
         print("[instagram] GitHub raw URL: " + video_url)
 
         container_url = f"https://graph.facebook.com/v21.0/{user_id}/media"
